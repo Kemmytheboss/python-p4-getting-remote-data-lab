5ba1b10348da1b8ed6d637116c4c8e3a64b1f02d
Verah Mokaya <verahmokaya475@gmail.com>
2026-01-08 09:43:53 +0300

    assignemnt done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,13 @@
-import requests
+import urllib.request
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        with urllib.request.urlopen(self.url) as response:
+            return response.read()
 
     def load_json(self):
-        pass
+        return json.loads(self.get_response_body())
